81b6c857b4b81fcdda112b2a0092a2ccb1e152c7
Lucian Fialho <lucianfialhobp@hotmail.com>
2023-01-25 23:27:50 -0300

    -- add conexao com banco de dados

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 require('dotenv').config();
 const FB = require('fb');
+const PrismaClient = require('@prisma/client').PrismaClient
 
-
+const prisma = new PrismaClient();
 (async () => {
-    // FB.setAccessToken(process.env.INSTAGRAM_TOKEN);
-    // FB.api('/17841401640520478/insights?metric=impressions&period=day', function (res) {
-    //     if(!res || res.error) {
-    //      console.log(!res ? 'error occurred' : res.error);
-    //      return;
-    //     }
-    //     console.log(res);
-    // });
+    console.log(await prisma.user.findMany())
+    FB.setAccessToken(process.env.INSTAGRAM_TOKEN);
+    FB.api('/17841401640520478/insights?metric=impressions,follower_count,website_clicks,reach&period=day', function (res) {
+        if(!res || res.error) {
+         console.log(!res ? 'error occurred' : res.error);
+         return;
+        }
+        console.log(res);
+    });
 
-    const response = await fetch(`https://graph.instagram.com/v15.0/17841401640520478?fields=id,username&access_token=${process.env.INSTAGRAM_TOKEN}`)
-    console.log(await response.json())
+    // const response = await fetch(`https://graph.instagram.com/v15.0/17841401640520478?fields=id,username&access_token=${process.env.INSTAGRAM_TOKEN}`)
+    // console.log(await response.json())
 })();
